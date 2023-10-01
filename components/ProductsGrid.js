cc6ce8c2457c05e066d02f0b8a387cab2c5f16ce
abdellatif-laghjaj <abdelatiflaghjaj@gmail.com>
2023-10-01 21:55:14 +0100

    Update ProductsGrid.js

--- components/ProductsGrid.js
+++ components/ProductsGrid.js
@@ -3,19 +3,25 @@ import ProductBox from "@/components/ProductBox";
 
 const StyledProductsGrid = styled.div`
   display: grid;
-  grid-template-columns: 1fr 1fr;
+  grid-template-columns: 1fr; /* Display one product per row by default */
   gap: 20px;
+
   @media screen and (min-width: 768px) {
-    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-template-columns: 1fr 1fr; /* Display two products per row on larger screens */
+  }
+
+  @media screen and (min-width: 1024px) {
+    grid-template-columns: 1fr 1fr 1fr 1fr; /* Display four products per row on even larger screens */
   }
 `;
 
-export default function ProductsGrid({products}) {
+export default function ProductsGrid({ products }) {
   return (
     <StyledProductsGrid>
-      {products?.length > 0 && products.map(product => (
-        <ProductBox key={product._id} {...product} />
-      ))}
+      {products?.length > 0 &&
+        products.map((product) => (
+          <ProductBox key={product._id} {...product} />
+        ))}
     </StyledProductsGrid>
   );
 }
